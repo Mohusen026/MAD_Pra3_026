2591d41d71a6256c29b2208810d98e6ffde70ad0
Mo_husen <mhusenhaji0@gmail.com>
2023-10-03 23:32:31 +0530

    MAD_Pra4_026

---
+++ app/src/main/java/com/mo_husen/mad_pra3_026/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mo_husen.mad_pra3_026
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
